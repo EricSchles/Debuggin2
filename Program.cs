a95f263869a47cccba56f5f3073dcbce09a7bae8
Eric Schles <ericschles@gmail.com>
2014-08-05 08:47:52 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debuggin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            a = a * 2;
+            a = a - 3;
+            a = a * 6;
+            Console.WriteLine(a);
+        }
+    }
+}
